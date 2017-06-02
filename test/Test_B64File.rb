cd228a5a019695ad9879e6c82a49f9e7a9d6d9ab
derekviera <ma.dmviera01@gmail.com>
2017-06-02 18:26:11 -0400

    Update tests

--- test/Test_B64File.rb
+++ test/Test_B64File.rb
@@ -3,10 +3,10 @@ require 'minitest/autorun'
 require File.join(File.dirname(__FILE__), '../lib/B64')
 # minitest test
 class TestB64 < Minitest::Test
+  include B64options
+  include TrollopOptions
   def setup
     @b64 = B64File.new
-    @b64.extend(B64options)
-   # @b64.extend(trollopoptions)
   end
 
   def test_time_equals_time_right_now?
@@ -14,7 +14,30 @@ class TestB64 < Minitest::Test
     assert_equal @time, @b64.time
   end
 
-  def test_start_includes_add?
-    assert_includes @b64.start, :add
+  def test_encode_is_vaild?
+    assert true, @b64.encode
+  end
+
+  def test_encode_writes_to_file?
+    write = @b64.encode('hello')
+    assert_equal write, @time
+    # clean up file
+    File.delete(Time.now.strftime('%Y-%m-%d_%H-%M-%S'))
+  end
+
+  def test_decode_file_nonexistent?
+    assert_raises(TypeError) { @b64.decode }
+  end
+
+  def test_decode_if_file_exists?
+    read = ['aSB3YW50IGEgY3JhY2tlciE=']
+    testfile = File.join(File.dirname(__FILE__), '../test/file/test.txt')
+    assert_equal read, @b64.decode(testfile)
+  end
+
+  def test_delete_is_vaild?
+    File.new 'delete', 'w'
+    delete = @b64.delete('delete')
+    assert true, delete
   end
 end
